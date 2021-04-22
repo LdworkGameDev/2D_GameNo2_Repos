f99246cd1a7136dbbb6d58b889c6b5dd09165f55
LdworkGameDev <ldwork.gamedesign1@gmail.com>
2021-04-22 23:10:45 +0700

    Audio manager

---
+++ 2D Game No.2/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Manager<SoundManager>
+{
+    public AudioClip sceneTheme;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PlayerSound();
+        }
+    }
+
+    public void PlayerSound()
+    {
+        GameObject soundOject = new GameObject();
+        AudioSource audioSource = soundOject.AddComponent<AudioSource>();
+        audioSource.PlayOneShot(sceneTheme);
+    }
+}
